9cadc46b7c1ea9682e26aeced411718e6c51ffe7
izhor <salomuz@naver.com>
2023-06-11 03:06:23 +0900

    examples

---
+++ 20 Valid Parenthese/ValidParenthese.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCTopInterviewQuestiona
+{
+    public class ValidParenthese
+    {
+         public bool isValid(string s)
+        {
+            if (s.Length % 2 != 0) return false;
+
+            Dictionary<char, char> parenthese = new Dictionary<char, char>()
+            {
+                { '(',')' },
+                { '[',']' },
+                { '{','}' },
+            };
+
+            Stack<char> Parentheses = new Stack<char>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+               if(s[i] == '(' || s[i] == '{' || s[i] == '[')
+                {
+                    Parentheses.Push(s[i]);
+                }
+                else if(s[i] == ')' || s[i] == '}' || s[i] == ']')
+                {
+                    if (Parentheses.Count == 0) return false;
+
+                    if(parenthese[Parentheses.Pop()] != s[i])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return Parentheses.Count == 0;
+        }
+    }
+}
